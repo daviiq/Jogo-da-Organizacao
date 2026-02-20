7571c07df9f7d42a3ec8add2112c553889a23d13
Davi Quirino <160233282+daviiq@users.noreply.github.com>
2026-02-19 22:11:27 -0300

    Continuação da interface gráfica em Java do jogo da organização, aprendendo sobre Swing + AWT, criações de botões, etc
    Updated the main class to create a welcome JFrame with buttons for game organization and finance graph.

--- Interface/Main.java
+++ Interface/Main.java
@@ -1,11 +1,48 @@
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("batata");
+        /* Molde para cria a página
+
+        Tela inicial
+        JFrame janelaPrincipal = new JFrame();
+        janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Tamanho da tela
+        janelaPrincipal.setSize(500,600);
+
+        //Adicionando botões:
+        JButton botaoSwing = new JButton("Botão Swing");
+        janelaPrincipal.add(botaoSwing);
+
+        //Adicionando Listener para capturar os eventos do botão Swing
+        botaoSwing.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Botão Swing pressionado");
+            }
+        });
+        janelaPrincipal.setVisible(true);}*/
+
+
+        //Tela inicial
+        JFrame janelaPrincipal = new JFrame("Bem Vindo ao Jogo da Evolução");
+        janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Tamanho da tela
+        janelaPrincipal.setSize(1920,1080);
+
+        //Layout da tela:
+        janelaPrincipal.setLayout(new FlowLayout());
+
+        janelaPrincipal.add(new JButton("Jogo Organização"));
+        janelaPrincipal.add(new JButton("Gráfico das finanças"));
+        janelaPrincipal.add(new JButton("Futuras implementações..."));
+
+        janelaPrincipal.setVisible(true);
     }
 }
